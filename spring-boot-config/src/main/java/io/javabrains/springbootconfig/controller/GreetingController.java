0eca645bbd28d2775439f70d58bf9318603b0cd9
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-04 17:17:35 -0500

    @configurationProperties

--- spring-boot-config/src/main/java/io/javabrains/springbootconfig/controller/GreetingController.java
+++ spring-boot-config/src/main/java/io/javabrains/springbootconfig/controller/GreetingController.java
@@ -1,18 +1,34 @@
 package io.javabrains.springbootconfig.controller;
 
+import io.javabrains.springbootconfig.config.DbSetting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController {
 
-    @Value("${my.greeting}")
+    @Value("${my.greeting: default valueee}")
   private String greetingMessage;
+
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+    @Value ("#{${dbValues}}")
+    private Map< String,String> mapvalue = new HashMap<>();
+
+    @Autowired
+    private DbSetting dbSetting;
+
     @GetMapping("/greeting")
     public String  greetings(){
-        return greetingMessage;
+        return greetingMessage + listValues + mapvalue +dbSetting.getHost() +dbSetting.getPort();
     }
 }
